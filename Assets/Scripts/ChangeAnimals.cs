72e3a8686b6022a0bace28576f3d33677b5fc100
Sergio Erik Madrigal Herrera <sergio.madrigal_smh@hotmail.com>
2015-08-14 16:27:27 -0500

    Primer Cambio
    Agregue modelados

---
+++ Assets/Scripts/ChangeAnimals.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class ChangeAnimals : MonoBehaviour {
+
+	public GameObject gameObject;
+	public GameObject gameObjectTucan;
+	public GameObject gameObjectViper;
+
+	public void Start(){
+		gameObject.SetActive (false);
+		gameObjectTucan.SetActive (false);
+		gameObjectViper.SetActive (false);
+	}
+	public void AnimalTiger(int TigerTo){
+	
+		gameObject.SetActive (true);
+		Debug.Log ("Activo");
+
+	}
+	public void AnimalTucan(int TigerTo){
+		
+		gameObjectTucan.SetActive (true);
+		Debug.Log ("Activo");
+	}
+	public void AnimalViper(int ViperTo){
+		
+		gameObjectViper.SetActive (true);
+		Debug.Log ("Activo");
+	}
+}
---
+++ Assets/Scripts/Movimient.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movimient : MonoBehaviour {
+
+
+	void OnMouseDown(){
+	
+			Vector3 temp = transform.position;
+			temp.y = 250;
+			transform.position = temp;
+		
+    }
+
+
+}
