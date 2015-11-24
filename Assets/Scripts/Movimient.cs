b420f47a8ddf0d253fccd726409b468d5ccdde55
Sergio Erik Madrigal Herrera <sergio.madrigal_smh@hotmail.com>
2015-11-24 13:39:03 -0600

    BestUI
    agregado

--- Assets/Scripts/Movimient.cs
+++ Assets/Scripts/Movimient.cs
@@ -3,14 +3,21 @@ using System.Collections;
 
 public class Movimient : MonoBehaviour {
 
+	public bool presionado;
 
+	Vector3 nuevo;
+
+	void Start(){
+		nuevo = transform.position;
+	}
 	void OnMouseDown(){
-	
-			Vector3 temp = transform.position;
-			temp.y = 250;
+		if(presionado){
+			presionado = false;
+			transform.position = nuevo;
+		}else{
+			presionado = true;
+			Vector3 temp = transform.position + transform.up*60;
 			transform.position = temp;
-		
+		}
     }
-
-
 }
---
+++ Assets/Scripts/Viper.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class Viper : MonoBehaviour,
+ITrackableEventHandler
+{
+	private TrackableBehaviour mTrackableBehaviour;
+	public AudioSource audio;
+	public Animation viperan;
+	
+	void Start()
+	{
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		if (mTrackableBehaviour)
+		{
+			mTrackableBehaviour.RegisterTrackableEventHandler(this);
+		}
+	}
+	
+	public void OnTrackableStateChanged(
+		TrackableBehaviour.Status previousStatus,
+		TrackableBehaviour.Status newStatus)
+	{
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+		    newStatus == TrackableBehaviour.Status.TRACKED ||
+		    newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+		{
+			// Play audio when target is found
+			audio.Play();
+			viperan.Play();
+		}
+		else
+		{
+			// Stop audio when target is lost
+			audio.Stop();
+			viperan.Stop();
+		}
+	}   
+}
